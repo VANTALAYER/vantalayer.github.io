837dc5f799e48715e4d0027dcd27209106335a27
VANTALAYER <vantalayer@gmail.com>
2026-02-20 02:41:56 -0500

    Change game title to section title in pages.js

--- js/pages.js
+++ js/pages.js
@@ -75,7 +75,7 @@ function renderHome(el) {
         </div>
         <div class="hero-meta">
           <div class="hero-title-block">
-            <div class="game-title">SHELLHACK</div>
+            <div class="section-title">SHELLHACK</div>
             <div class="game-status">&#x25CF;&nbsp; Under Development &mdash; Coming Soon</div>
           </div>
           <a href="${LINKS.reddit}" target="_blank" rel="noopener" class="btn-community">
